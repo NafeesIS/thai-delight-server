5719bda40a01ff2da11d69507c18f56ddd799fdf
NafeesIS <nafeesshwaon555@gmail.com>
2023-06-22 18:47:17 +0600

    another commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(\())
 const categories = require('./data/categories.json')
 console.log(categories);
 
